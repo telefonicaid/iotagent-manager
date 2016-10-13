e959482e0ff02fe98e83d978a14ae7aa80b8014c
Alvaro Vega <avega@tid.es>
2016-10-13 14:40:36 +0200

    replace equal 8 by greaterThan 6

--- test/unit/configuration-retrieval-test.js
+++ test/unit/configuration-retrieval-test.js
@@ -127,7 +127,7 @@ describe('Configuration list', function() {
             request(options, function(error, response, body) {
                 var parsedBody = JSON.parse(body);
 
-                parsedBody.services.length.should.greaterThan(7);
+                parsedBody.services.length.should.greaterThan(5);
                 done();
             });
         });
